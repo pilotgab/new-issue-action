7affa420156ba1e68d7818e3aafc1d0dda9d0297
alialaa <hello@alialaa.com>
2023-07-31 17:35:42 +0300

    removed sourcemaps

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
+/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
 /***/ 7351:
@@ -9812,4 +9812,3 @@ run();
 module.exports = __webpack_exports__;
 /******/ })()
 ;
-//# sourceMappingURL=index.js.map
